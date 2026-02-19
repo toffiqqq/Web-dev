e36323f9bbc62592058f976f1bb31a368fbc9678
Artyom <artyomkn@gmail.com>
2026-02-19 12:38:21 +0500

    r

--- lab3/task1/chapter2/basicOperators.js
+++
@@ -1,8 +0,0 @@
-let a = 1, b = 1;
-let c = ++a; 
-let d = b++;
-
-let x = prompt("First number?", 1);
-let y = prompt("Second number?", 2);
-
-alert(x + y);
--- lab3/task1/chapter3/debug.js
+++
@@ -1,7 +0,0 @@
-function hello(name) {
-  let phrase = `Hello, ${name}!`;
-
-  debugger; 
-
-  say(phrase);
-}
